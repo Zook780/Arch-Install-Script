a64478ad7dfe0fea953786ed2d757b59b840abe6
Zook780 <virajlalsare9@gmail.com>
2022-03-11 18:12:31 +0530

    pls work this time bruh pls pls plox ;( ;( ;(

--- install_part2.sh
+++ install_part2.sh
@@ -23,49 +23,18 @@ usermod -aG wheel,audio,network,video,optical,storage $username
 echo "set user password"
 passwd $username
 echo ""
-echo "Installing packages"
+echo "installing packages"
 ech0 ""
 pacman -S --noconfirm xorg git xorg-server xorg-xinit sxiv mpv \
        zathura zathura-pdf-poppler nitrogen ffmpeg ncmpcpp python-pywal \
        mutt qt5ct ranger unzip zip maim imagemagick fzf youtube-dl \
        firefox base-devel networkmanager zsh pamixer noto-fonts pulseaudio pavucontrol \
        alsa-utils vim sudo emacs zip unzip git libnotify noto-fonts-emoji noto-fonts-cjk\
-       notify-osd picom sudo bluez-utils xdg-user-dirs xf86-video-fbdev \
+       notify-osd picom vim sudo bluez-utils xdg-user-dirs xf86-video-fbdev \
        xf86-video-intel xf86-video-nouveau xf86-video-openchrome
 systemctl enable NetworkManager
 vim /etc/sudoers
 printf '\033c'
-cd ~
-git clone https://github.com/Zook780/dotfiles
-# for some reasons i was not able to get all the files by copying from dotfiles to ./
-cp -r dotfiles/.config ./
-cp -r dotfiles/.dmenu ./
-cp -r dotfiles/.dwm ./
-cp -r dotfiles/.dwmblocks ./
-cp -r dotfiles/.emacs.d ./
-cp -r dotfiles/.git ./
-cp -r dotfiles/.oh-my-zsh ./
-cp -r dotfiles/.st ./
-cp -r dotfiles/.vim ./
-rm dotfiles/LICENSE
-rm dotfiles/README.md
-cp -r dotfiles/.Xresources ./
-cp -r dotfiles/.gitignore ./
-cp -r dotfiles/.xinitrc ./
-cp -r dotfiles/.zlogin ./
-cp -r dotfiles/.zshrc ./
-rm -rf dotfiles/img
-cp -r dotfiles/pix ./
-rm -rf dotfiles/
-wal -i pix/wall/abstract-face.png
-sudo make -C ~/.dwm/ clean install
-sudo make -C ~/.dwmblocks/ clean install
-sudo make -C ~/.st/ clean install
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git/
-makepkg -si
-yay --noconfirm -S pfetch tela-icon-theme ttf-mononoki ttf-jetbrains-mono
-rm -rf img/
 chsh -s /usr/bin/zsh
 pacman --noconfirm -S grub mtools efibootmgr os-prober dosfstools
 echo "enter EFI partition" 
---
+++ install_part3.sh
@@ -0,0 +1,32 @@
+echo "change #include path in .dwm .dwmblocks .st .dwmblocks (press enter)"
+read "stop"
+sudo make -C ~/.dwm/ clean install
+sudo make -C ~/.dwmblocks/ clean install
+sudo make -C ~/.st/ clean install
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git/
+makepkg -si
+yay --noconfirm -S pfetch tela-icon-theme ttf-mononoki ttf-jetbrains-mono
+git clone https://github.com/Zook780/dotfiles
+# for some reasons i was not able to get all the files by copying from dotfiles to ./
+cp -r dotfiles/.config ./
+cp -r dotfiles/.dmenu ./
+cp -r dotfiles/.dwm ./
+cp -r dotfiles/.dwmblocks ./
+cp -r dotfiles/.emacs.d ./
+cp -r dotfiles/.git ./
+cp -r dotfiles/.oh-my-zsh ./
+cp -r dotfiles/.st ./
+cp -r dotfiles/.vim ./
+rm dotfiles/LICENSE
+rm dotfiles/README.md
+cp -r dotfiles/.Xresources ./
+cp -r dotfiles/.gitignore ./
+cp -r dotfiles/.xinitrc ./
+cp -r dotfiles/.zlogin ./
+cp -r dotfiles/.zshrc ./
+rm -rf dotfiles/img
+cp -r dotfiles/pix ./
+rm -rf dotfiles/
+wal -i pix/wall/abstract-face.png
+rm -rf img/

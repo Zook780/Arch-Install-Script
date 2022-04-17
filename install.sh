10eed970e641cede652e8a925851d2de03226463
Zook780 <virajlalsare9@gmail.com>
2022-04-17 18:13:56 +0530

    added some comments and spacing

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # !/bin/sh
+
 #  _____           _    _          _             _
 # |__  /___   ___ | | _( )___     / \   _ __ ___| |__"
 #   / // _ \ / _ \| |/ /// __|   / _ \ | '__/ __| '_ \"
@@ -14,6 +15,7 @@
 
 printf '\033c'
 
+# some bullshit
 echo "Hi you want to install arch linux with my dotfiles :flush_emoji:"
 echo ""
 echo "Make sure you have network :)"
@@ -24,26 +26,40 @@ echo "Do you want to continue ? press enter"
 read hmm
 timedatectl set-ntp true
 printf '\033c'
+
+# partition your disk
 cfdisk /dev/sda  
+
 lsblk
+
+# format the boot partition
 echo "type your boot/efi partition"
 read efi
 mkfs.fat -F32 $efi
 echo ""
 echo ""
+
+# format swap partition
 echo "type your swap partition"
 read swap
 echo ""
 mkswap $swap
 swapon $swap
+
+# format root partition
 echo ""
 echo "type your root partition"
 read root
 mkfs.ext4 $root
 mount $root /mnt
+
+# install the basic system (kernel, etc..) to /mnt
 pacstrap /mnt linux base linux-firmware
+
+# generate file system table
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "ParallelDownloads = 5" >> /etc/pacman.conf
+
+# run the 2nd script
 chmod +x ./install_part2.sh
 cp install_part2.sh /mnt/
-arch-chroot /mnt ./install_part2.sh
+arch-chroot /mnt ./install_part2.sh ; rm /mnt/install_part2.sh
--- install_part3.sh
+++ install_part3.sh
@@ -1,22 +1,34 @@
 echo "change #include path in .dwm .dwmblocks .st .dwmblocks"
 read yes
+
+# install yay 
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
 makepkg -si
+
+# install some fonts and pfetch(minimal neofetch)
 yay --noconfirm -S pfetch tela-icon-theme ttf-font-awesome ttf-mononoki ttf-jetbrains-mono
+
 # cloudflare and google dns 
 sudo echo "nameserver 1.1.1.1" >> /etc/resolv.conf 
 sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
 sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+# make the file write protected so that network manager doesn't override it
 sudo chattr +i /etc/resolv.conf
-# for some reason packages were not able to install 
+
+# for some reason packages were not able to install when aligned with indentation and /
 pacman -S --noconfirm xorg git xorg-server xorg-xinit sxiv mpv mpd zathura zathura-pdf-poppler nitrogen ffmpeg ncmpcpp python-pywal redshift ranger unzip zip maim imagemagick fzf youtube-dl newsboat pulseaudio firefox neomutt base-devel networkmanager zsh pamixer noto-fonts pavucontrol alsa-utils emacs qt5ct git libnotify blueberry noto-fonts-emoji noto-fonts-cjk vim dunst picom sudo bluez-utils xdg-user-dirs xf86-video-fbdev ueberzug lxappearance xf86-video-intel xf86-video-nouveau xf86-video-openchrome
+
+# clone the dotfiles
 git clone https://github.com/Zook780/dotfiles
 cd dotfiles
 mv * ../
 cd
 rm -rf dotfiles/
+
+# compile dwm, st, dmenu and dwmblocks
 wal -i pix/wall/abstract-face.png
 sudo make -C ~/.dwm/ clean install
+sudo make -C ~/.dmenu/ clean install
 sudo make -C ~/.dwmblocks/ clean install
 sudo make -C ~/.st/ clean install
